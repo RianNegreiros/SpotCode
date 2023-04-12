43cf986d8e32aed25f731caa0b1badcbb1ecadde
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-12 18:05:35 -0300

    fix typo

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protected_from_forgery unless: -> { request.format.json? }
+  protect_from_forgery unless: -> { request.format.json? }
 end
