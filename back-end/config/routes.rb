a06bf9a87294f17f1966c1ccbf5fa0581afd0df8
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-12 14:10:50 -0300

    create dashboard endpoints and fix webpack error

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'home/index'
   root 'home#index'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: [:index]
+    end
+  end
 end
