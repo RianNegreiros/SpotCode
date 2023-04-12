d29a481017cae43823e922195d9d829dfd5a50fd
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-12 16:54:12 -0300

    create GET album endpoint

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :dashboard, only: [:index]
       resources :categories, only: [:index, :show]
       resources :search, only: [:index]
+      resources :albums, only: [:show]
     end
   end
 end
