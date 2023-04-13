256980d3f71f7facf9c3b68ec4db99ce23774a63
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-13 11:39:26 -0300

    add menu

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -3,11 +3,13 @@ import 'react-bulma-components/dist/react-bulma-components.min.css'
 import 'app.scss'
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
+import Menu from './components/commom/menu'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Menu />
         <Routes />
       </BrowserRouter>
     </>
--- app/javascript/packs/spotify-clone.jsx
+++ app/javascript/packs/spotify-clone.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../app'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   ReactDOM.render(
     <App />,
     document.getElementById('root'),
