3ca6b05d9c9626c284209b5785a2ca4f7a3ab731
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-13 10:09:23 -0300

    add bulma

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import 'react-bulma-components/dist/react-bulma-components.min.css'
+import 'app.scss'
 
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <div>Test</div>
     </>
   )
 }
