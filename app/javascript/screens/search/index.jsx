2abeefddef4c06634e1fc978a251300a176c2d96
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-14 16:35:06 -0300

    build search screen

--- app/javascript/screens/search/index.jsx
+++ app/javascript/screens/search/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import SectionWrapper from '../../components/common/section_wrapper'
+import { Heading } from 'react-bulma-components'
+import Search from '../../components/search'
 import NavbarFooter from '../../components/common/navbar_footer'
 
 export default function SearchScreen() {
   return (
     <>
-      <h1>Search</h1>
+      <SectionWrapper>
+        <Heading className='has-text-centered has-text-white'>Search</Heading>
+        <Search />
+      </SectionWrapper>
       <NavbarFooter />
     </>
   )
