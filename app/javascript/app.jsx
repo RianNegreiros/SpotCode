1a804e38585641e531df82c4e58ac48cc27ece55
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-14 17:08:28 -0300

    fix typo on menu import

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import 'react-bulma-components/dist/react-bulma-components.min.css'
 import 'app.scss'
-import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
-import Menu from './components/commom/menu'
+import { BrowserRouter } from 'react-router-dom'
+import Menu from './components/common/menu'
 
 export default function App() {
   return (
