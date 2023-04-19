49c93d90cded9182fa3884f46620830eca65bc8d
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-19 19:09:08 -0300

    fix #4

--- app/javascript/packs/spotify-clone.jsx
+++ app/javascript/packs/spotify-clone.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from '../app'
 
 document.addEventListener('turbolinks:load', () => {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root'),
-  )
+  const container = document.getElementById('root');
+  const root = createRoot(container);
+  root.render(<App />);
 })
