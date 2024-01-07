b797d3da1e45523c8306b2faaece01bda26059c6
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-07 10:46:07 -0300

    feat: add create user method to user service

--- src/main/java/com/github/riannegreiros/springcloud/springcloud/services/UserService.java
+++ src/main/java/com/github/riannegreiros/springcloud/springcloud/services/UserService.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -16,6 +19,19 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User createUser(String name, String email, String password) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+
+        return repository.save(user);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
