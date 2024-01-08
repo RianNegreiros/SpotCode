299a6197ddab0b56ccc26c51597b9ac43446e3f5
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-08 04:25:38 -0300

    feat: implement category controller

--- src/main/java/com/github/riannegreiros/springcloud/springcloud/controllers/CategoryController.java
+++ src/main/java/com/github/riannegreiros/springcloud/springcloud/controllers/CategoryController.java
@@ -1,4 +1,29 @@
 package com.github.riannegreiros.springcloud.springcloud.controllers;
 
+import com.github.riannegreiros.springcloud.springcloud.entities.Category;
+import com.github.riannegreiros.springcloud.springcloud.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
+    @Autowired
+    CategoryService service;
+
+    @GetMapping
+    public List<Category> getAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
 }
